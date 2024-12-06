377d2467417170d4d9a100097d3173be8994c518
Giancarlo <giancarlo.lca@gmail.com>
2024-12-05 21:24:25 -0300

    Exportando casos de uso

--- packages/core/src/permissao/index.ts
+++ packages/core/src/permissao/index.ts
@@ -1,3 +1,8 @@
 import Permissao from "./model/Permissao.ts";
+import AtualizarPermissao from "./service/AtualizarPermissao.ts";
+import ExcluirPermissao from "./service/ExcluirPermissao";
+import ObterTodasPermissao from "./service/ObterTodasPermissao.ts";
+import RegistrarPermissao from "./service/RegistrarPermissao.ts";
 
 export type { Permissao }
+export { AtualizarPermissao, ExcluirPermissao, ObterTodasPermissao, RegistrarPermissao };
--- packages/core/src/usuario/index.ts
+++ packages/core/src/usuario/index.ts
@@ -1,3 +1,7 @@
 import Usuario from "./model/Usuario";
+import LoginUsuario from "./service/LoginUsusario.ts";
+import RegistrarUsuario from "./service/RegistrarUsuario.ts";
+
 
 export type { Usuario }
+export { LoginUsuario, RegistrarUsuario };
